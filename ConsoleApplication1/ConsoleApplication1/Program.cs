c112243d8851ee49d60b607f00bbf09b43c2d656
Lucas Machado <lucmoliveira@gmail.com>
2013-06-04 11:08:47 -0300

    teste
    teste

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string teste = Regex.Match("123456789001235677888", @"\d{11}").Value;
+            string teste2 = Regex.Match("4", @"\d{11}").Value;
+
+            string msg = "Cbahia06630459679";
+            msg = msg.Replace(".", "").Replace("-", "");
+            Match match = Regex.Match(msg, @"(?<cpf>\d{11})");
+            string cpf = match.Groups["cpf"].Value;
+
+            if (ValidateCPF(cpf)) 
+            {
+                System.Console.WriteLine("05".PadLeft(8, '0'));
+            }
+
+            System.Console.WriteLine("05".PadLeft(8, '0'));
+            System.Console.WriteLine("05".PadRight(8, '0'));
+        }
+        static private bool ValidateCPF(string cpf)
+        {
+            int[] multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            string tempCpf;
+            string digito;
+            int soma;
+            int resto;
+
+            cpf = cpf.Trim();
+
+            if (cpf.Length != 11)
+                return false;
+
+            switch (cpf)
+            {
+                case "11111111111":
+                    return false;
+                case "00000000000":
+                    return false;
+                case "2222222222":
+                    return false;
+                case "33333333333":
+                    return false;
+                case "44444444444":
+                    return false;
+                case "55555555555":
+                    return false;
+                case "66666666666":
+                    return false;
+                case "77777777777":
+                    return false;
+                case "88888888888":
+                    return false;
+                case "99999999999":
+                    return false;
+            }
+
+            tempCpf = cpf.Substring(0, 9);
+            soma = 0;
+            for (int i = 0; i < 9; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
+
+            resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+
+            digito = resto.ToString();
+
+            tempCpf = tempCpf + digito;
+
+            soma = 0;
+            for (int i = 0; i < 10; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
+
+            resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+
+            digito = digito + resto.ToString();
+
+            return cpf.EndsWith(digito);
+        }
+
+    }
+}
